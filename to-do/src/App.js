62f09f0066f7bc3c0aca88521e47efa50f80887c
Babatunde <babatundea15@gmail.com>
2019-12-10 07:52:43 +0100

    I created the onSubmit and onChange methods and defined them

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       items: [],
+       currentItem: {
+         text: '',
+         key: '',
+       }
+    }
+  }
+  
   render(){
     return(
       <div className="App">
       <header>
-        <form id = "to-do-form">
-          <input type="text" placeholder="Enter Task"/>
+        <form id = "to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Task" value={this.state.currentItem.text} onChange={this.handleInput}/>
           <button type="submit">ADD TASK</button>
         </form>
       </header>
