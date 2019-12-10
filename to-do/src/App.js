6329125e9e54bd5b45130cdee9efcbef8498e75f
Babatunde <babatundea15@gmail.com>
2019-12-10 18:01:05 +0100

    new commit

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,31 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ListTask from './Components/ListTask'
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       items: [],
-       currentItem: {
-         text: '',
-         key: '',
-       }
-    }
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    };
   }
-  
-  render(){
-    return(
+
+  handleInput = e => {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    });
+  };
+
+  addItem = e => {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: "",
+          key: ""
+        }
+      });
+    }
+  };
+  render() {
+    return (
       <div className="App">
-      <header>
-        <form id = "to-do-form" onSubmit={this.addItem}>
-          <input type="text" placeholder="Enter Task" value={this.state.currentItem.text} onChange={this.handleInput}/>
-          <button type="submit">ADD TASK</button>
-        </form>
-      </header>
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Task"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <button type="submit">ADD TASK</button>
+          </form>
+        </header>
+        <ListTask items={this.state.items}/>
       </div>
-    )
+    );
   }
 }
 
