a855ff482b90ea6c34a02b17c9a036df5c2d86ee
Babatunde <babatundea15@gmail.com>
2019-12-11 03:10:41 +0100

    I added animation

--- to-do/src/Components/ListTask.js
+++ to-do/src/Components/ListTask.js
@@ -1,6 +1,6 @@
 import React from "react";
-import '../Components/Listitem.css'
-
+import "../Components/Listitem.css";
+import FlipMove from "react-flip-move";
 
 function ListTask(props) {
   const items = props.items;
@@ -8,20 +8,30 @@ function ListTask(props) {
     return (
       <div className="list" key={item.key}>
         <p>
-        <input type="text" 
-        id ={item.key} 
-        value={item.text}
-        onChange={(e)=>{
-            props.setUpdate(e.target.value, item.key)
-        }}/>
-        <span>
-        <button className="btn" onClick={()=>props.deleteTask(item.key)}>CLEAR</button>
-        </span>
+          <input
+            type="text"
+            id={item.key}
+            value={item.text}
+            onChange={e => {
+              props.setUpdate(e.target.value, item.key);
+            }}
+          />
+          <span>
+            <button className="btn" onClick={() => props.deleteTask(item.key)}>
+              CLEAR
+            </button>
+          </span>
         </p>
       </div>
     );
   });
-  return <div>{listItems}</div>;
+  return (
+    <div>
+      <FlipMove duration={500} easing="ease-in-out">
+        {listItems}
+      </FlipMove>
+    </div>
+  );
 }
 
 export default ListTask;
